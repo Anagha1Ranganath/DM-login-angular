007452a70f8aec7c69f53b36feb2c38c43e5babf
Anagha <cranagh1@in.ibm.com>
2019-02-14 06:22:36 +0530

    second commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
     HeaderComponent,
